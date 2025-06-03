a6a6cc7f2d6c197ba761fd21f8f4242f1e3f68ba
Johnny Anderson Nascimento <nascimentoho1998@gmail.com>
2025-06-03 13:27:07 -0300

    - Implementado propriedades faltantes e sobrescrito método instalarAplicativo.

--- Program.cs
+++ Program.cs
@@ -1,3 +1,13 @@
 ﻿using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+Console.WriteLine("IPHONE");
+Iphone iphone = new Iphone("1234", "5", "1234", 64);
+iphone.Ligar();
+iphone.ReceberLigacao();
+iphone.InstalarAplicativo("Angry Birds");
+
+Console.WriteLine("NOKIA");
+Nokia nokia = new Nokia("567", "10", "567", 32);
+nokia.Ligar();
+nokia.ReceberLigacao();
+nokia.InstalarAplicativo("Angry Birds");
